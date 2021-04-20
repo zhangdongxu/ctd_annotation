da55cf4c81b201a13c7b8c59f697270ea7ec7407
Dongxu <zhangdongxuu@gmail.com>
2021-04-20 01:17:15 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,3 @@
+var external_data = [{"text":"cat love dog, bird hates eagle", "pid":"123", "entities": ["cat", "dog", "bird", "eagle"]},
+{"text":"2cat love dog, bird hates eagle", "pid":"234", "entities": ["2cat", "dog", "bird", "eagle"]},
+{"text":"3cat love dog, bird hates eagle", "pid":"345", "entities": ["3cat", "dog", "bird", "eagle"]}];
---
+++ functions.js
@@ -0,0 +1,200 @@
+var input_data = external_data;
+      var page_index = -1;
+      var num_labels = 0;
+      var pid = "";
+      var label_list = [["", "", ""]];
+
+      //   function loadData() {
+      //     fetch("./data.json")
+      //       .then(function (res) {
+      //         return res.json();
+      //       })
+      //       .then(function (data) {
+      //         for (var i = 0; i < data.length; i++) {
+      //           input_data.push(data[i]);
+      //         }
+      //       });
+      //   }
+
+      function update_new_page() {
+        document.getElementById("messages").innerHTML =
+          "Page " + (page_index + 1);
+        pid = input_data[page_index]["pid"];
+        document.getElementById("pubmedid").innerHTML = "PID: " + pid;
+        let abstract = input_data[page_index]["text"];
+        document.getElementById("pubmed_text").innerHTML = abstract;
+        generate_annotation_buttons((clear_history = true));
+      }
+
+      function changeLabel(slot) {
+        console.log(slot);
+        slot_type = slot.id.slice(0, 2);
+        label_index = parseInt(slot.id.slice(2));
+
+        console.log(slot_type);
+        console.log(label_index);
+        console.log(slot[slot.selectedIndex].text);
+        if (slot_type == "e1") {
+          label_list[label_index][0] = slot[slot.selectedIndex].text;
+          console.log("update e1");
+        } else if (slot_type == "e2") {
+          label_list[label_index][1] = slot[slot.selectedIndex].text;
+          console.log("update e2");
+        } else {
+          label_list[label_index][2] = slot[slot.selectedIndex].text;
+          console.log("update re");
+        }
+      }
+
+      function generate_annotation_buttons(clear_history = false) {
+        if (clear_history == false) {
+          label_list.push(["", "", ""]);
+        } else {
+          label_list = [["", "", ""]];
+        }
+        let entities_current_page = input_data[page_index]["entities"];
+        let relation_types = [
+          "chem_gene:effect",
+          "chem_disease:effect",
+          "gene_disease:effect",
+        ];
+
+        let appended_options = "";
+        for (var i = 0; i < label_list.length; i++) {
+          let entity1options = "<option value='0'></option>";
+          for (var j = 0; j < entities_current_page.length; j++) {
+            let default_selection = "";
+            if (entities_current_page[j] == label_list[i][0]) {
+              default_selection = "' selected = 'selected'";
+            }
+            entity1options +=
+              "<option value='entity" +
+              j +
+              default_selection +
+              "'>" +
+              entities_current_page[j] +
+              "</option>";
+          }
+
+          let entity2options = "<option value='0'></option>";
+          for (var j = 0; j < entities_current_page.length; j++) {
+            let default_selection = "";
+            if (entities_current_page[j] == label_list[i][1]) {
+              default_selection = "' selected = 'selected'";
+            }
+            entity2options +=
+              "<option value='entity" +
+              j +
+              default_selection +
+              "'>" +
+              entities_current_page[j] +
+              "</option>";
+          }
+
+          let relationoptions = "<option value='0'></option>";
+          for (var j = 0; j < relation_types.length; j++) {
+            let default_selection = "";
+            if (relation_types[j] == label_list[i][2]) {
+              default_selection = "' selected = 'selected'";
+            }
+            relationoptions +=
+              "<option value='relation" +
+              j +
+              default_selection +
+              "'>" +
+              relation_types[j] +
+              "</option>";
+          }
+
+          let e1options =
+            'E1:<select id="e1' +
+            i +
+            '" onchange=changeLabel(e1' +
+            i +
+            ")>" +
+            entity1options +
+            "</select>";
+          let e2options =
+            ' E2:<select id="e2' +
+            i +
+            '" onchange=changeLabel(e2' +
+            i +
+            ")>" +
+            entity2options +
+            "</select>";
+          let reloptions =
+            ' Relation label:<select class="relation", id="re' +
+            i +
+            '" onchange=changeLabel(re' +
+            i +
+            ")>" +
+            relationoptions +
+            "</select>";
+          appended_options =
+            appended_options + e1options + e2options + reloptions + "<br>";
+        }
+        console.log(appended_options);
+        document.getElementById("options").innerHTML = appended_options;
+        num_labels++;
+      }
+
+      function previousPage() {
+        page_index--;
+        if (page_index < 0) {
+          document.getElementById("messages").innerHTML =
+            "Already the first page!";
+          page_index = 0;
+          return null;
+        } else {
+          num_labels = 0;
+          update_new_page();
+        }
+      }
+
+      function nextPage() {
+        page_index++;
+        if (page_index >= input_data.length) {
+          document.getElementById("messages").innerHTML =
+            "Already the last page.";
+          page_index = input_data.length - 1;
+          return null;
+        } else {
+          num_labels = 0;
+          update_new_page();
+        }
+      }
+
+      function gotoPage() {
+        pagenumber = document.getElementById("inputpage").value;
+        // if (pagenumber == null) {
+        //   return null;
+        // }
+        let page_index_ = parseInt(pagenumber);
+        if (isNaN(page_index_)) {
+          document.getElementById("messages").innerHTML =
+            "Please input an integer";
+          page_index = input_data.length - 1;
+          return null;
+        } else {
+          page_index = page_index_ - 1;
+          num_labels = 0;
+        }
+        if (page_index >= input_data.length) {
+          page_index = input_data.length - 1;
+        } else if (page_index <= 0) {
+          page_index = 0;
+        }
+        update_new_page();
+      }
+
+      function add_label() {
+        generate_annotation_buttons();
+      }
+
+      function save_label() {
+        console.log(label_list);
+        // var blob = new Blob([label_list], { type: "application/json" });
+        // saveAs(blob, pid + ".json");
+        var storedItem = localStorage.getItem("storedItem");
+        localStorage.setItem("storedItem", label_list);
+      }
