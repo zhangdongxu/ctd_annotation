865d8b84fee3af81cb96e8fc57bbf82faa44d914
Dongxu <zhangdongxuu@gmail.com>
2021-04-21 03:45:00 -0400

    updates

--- data.js
+++
@@ -1,3 +0,0 @@
-var external_data = [{"text":"cat love dog, bird hates eagle", "pid":"123", "entities": ["cat", "dog", "bird", "eagle"]},
-{"text":"2cat love dog, bird hates eagle", "pid":"234", "entities": ["2cat", "dog", "bird", "eagle"]},
-{"text":"3cat love dog, bird hates eagle", "pid":"345", "entities": ["3cat", "dog", "bird", "eagle"]}];
