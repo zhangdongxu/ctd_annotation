2adbb018eafbfe37b61af85a9c93eea9f2dc34e8
Dongxu <zhangdongxuu@gmail.com>
2021-04-26 12:55:52 -0400

    add annotation database

--- convert_nedb_format.js
+++ convert_nedb_format.js
@@ -1,3 +1,22 @@
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 const Datastore = require('nedb');
 let database = new Datastore('data.db');
 database.loadDatabase();
@@ -6,8 +25,12 @@ database.insert({account_info: true, username: 'apple', browsing_history: 0});
 database.insert({account_info: true, username: 'banana', browsing_history: 0});
 
 const fs = require('fs');
-let rawdata = fs.readFileSync('test.json');
-let input_data = JSON.parse(rawdata);
+let rawdata1 = fs.readFileSync('valid.json');
+let input_data1 = JSON.parse(rawdata1);
+let rawdata2 = fs.readFileSync('test.json');
+let input_data2 = JSON.parse(rawdata2);
+let input_data = input_data1.concat(input_data2);
+shuffle(input_data);
 let type_set = {"Chemical": null, "Gene": null, "Disease": null};
 
 for (var i=0; i < input_data.length; i++){
